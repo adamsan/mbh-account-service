502e501bb8fbb368dfd61125b817fc3ed7e52994
Szentirmai-Schon Adam <szentirmai.adam@lpsolutions.hu>
2024-03-27 22:32:15 +0100

    basic account custom id generation with prefix is working

--- src/main/kotlin/hu/mbhbank/accountservice/AccountsController.kt
+++ src/main/kotlin/hu/mbhbank/accountservice/AccountsController.kt
@@ -4,14 +4,17 @@ import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
-import jakarta.persistence.SequenceGenerator
+import org.hibernate.annotations.GenericGenerator
+import org.hibernate.annotations.Parameter
+import org.hibernate.engine.spi.SharedSessionContractImplementor
+import org.hibernate.id.enhanced.SequenceStyleGenerator
+import org.hibernate.service.ServiceRegistry
+import org.hibernate.type.Type
+import org.hibernate.type.spi.TypeConfiguration
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import java.util.*
 
 @RestController
 @RequestMapping("/api/v1/account")
@@ -31,8 +34,12 @@ class AccountsController(@Autowired private val accountsRepository: AccountsRepo
 @Entity(name = "accounts")
 data class Account(
         @Id
-        @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "account_number_generator")
-        @SequenceGenerator(name="account_number_generator", sequenceName = "accounts_seq", allocationSize = 1)
+        @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "accounts_seq")
+        @GenericGenerator(
+                name = "accounts_seq",
+                strategy = "hu.mbhbank.accountservice.CustomIdGenerator",
+                parameters = [Parameter(name = "increment_size", value = "1")]
+        )
         val accountNumber: Long?,
         val accountHolderName: String,
         val isDeleted: Boolean = false) {}
@@ -40,3 +47,16 @@ data class Account(
 interface AccountsRepository : JpaRepository<Account, Long> {}
 
 data class NewAccountDTO(val accountHolderName: String)
+
+class CustomIdGenerator(): SequenceStyleGenerator() {
+    val prefix = "1234000"
+    override fun generate(session: SharedSessionContractImplementor?, `object`: Any?): Any {
+        return (prefix + super.generate(session, `object`)).toLong()
+    }
+
+    override fun configure(type: Type?, parameters: Properties?, serviceRegistry: ServiceRegistry?) {
+        super.configure(TypeConfiguration().basicTypeRegistry.getRegisteredType(Long::class.java), parameters, serviceRegistry)
+        parameters?.setProperty(SEQUENCE_PARAM, "accounts_seq")
+        parameters?.setProperty(INCREMENT_PARAM, "1")
+    }
+}
