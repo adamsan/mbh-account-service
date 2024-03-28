cb0990f2cfca5e747a61369888c3fc09a000ce57
Szentirmai-Schon Adam <szentirmai.adam@lpsolutions.hu>
2024-03-28 13:25:39 +0100

    post, get by id, put implemented for transactions

--- src/main/kotlin/hu/mbhbank/accountservice/transactions/controller/TransactionsController.kt
+++ src/main/kotlin/hu/mbhbank/accountservice/transactions/controller/TransactionsController.kt
@@ -1,11 +1,10 @@
 package hu.mbhbank.accountservice.transactions.controller
 
 import jakarta.persistence.*
+import org.hibernate.annotations.CreationTimestamp
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.math.BigDecimal
 import java.time.LocalDateTime
 import java.util.UUID
@@ -16,6 +15,28 @@ class TransactionsController(@Autowired private val transactionsRepository: Tran
 
     @GetMapping
     public fun get(): List<Transaction> = transactionsRepository.findAll()
+
+    @PostMapping
+    fun create(@RequestBody transaction: Transaction): Transaction {
+        return transactionsRepository.save(transaction.copy(uuid = UUID.randomUUID()))
+    }
+
+    @GetMapping("/{uuid}")
+    fun getById(@PathVariable("uuid") uuid: UUID): Transaction {
+        return transactionsRepository.findById(uuid).orElseThrow { NoSuchElementException("Transaction not found") }
+    }
+
+    @PutMapping("/{uuid}")
+    fun update(@PathVariable("uuid") uuid: UUID, @RequestBody updatedTransaction: Transaction): Transaction {
+        val existingTransaction = transactionsRepository.findById(uuid).orElseThrow { NoSuchElementException("Transaction not found") }
+        val mergedTransaction = existingTransaction.copy(
+                accountNumber = updatedTransaction.accountNumber,
+                type = updatedTransaction.type,
+                amount = updatedTransaction.amount,
+                timestamp = updatedTransaction.timestamp
+        )
+        return transactionsRepository.save(mergedTransaction)
+    }
 }
 
 @Entity(name = "transactions")
@@ -27,8 +48,8 @@ data class Transaction(
         @Enumerated(EnumType.ORDINAL)
         val type: Type,
         val amount: Long,
-        @Column(name = "timestamp", nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
-        val timestamp: LocalDateTime
+        @CreationTimestamp
+        val timestamp: LocalDateTime?
 ) {}
 
 enum class Type {
